bd2dd7a41e89888665e142c45b4b5ad74bbed11e
MickaelCRL <ce.mickael27@gmail.com>
2024-01-07 12:54:52 +0100

    mode de paiement

--- src/main/php/static/js/verifPaiement.js
+++ src/main/php/static/js/verifPaiement.js
@@ -1,44 +1,8 @@
-var choixPaiement = "";
-
-function selectPayment(paymentType) {
-  choixPaiement = paymentType;
-
-  var response = confirm(
-    "Confirmez-vous votre choix de paiement : " + choixPaiement + " ?"
-  );
-  if (response) {
-    alert("Paiement confirmé");
-    $.ajax({
-      type: "POST",
-      url: "../../actions/addModePaiement.php",
-      data: { choixPaiement: choixPaiement },
-    });
-  } else {
-    choixPaiement = "";
-  }
-}
-
-function enregistrer() {
-  enregistrerClique = true;
-  alert("Informations enregistrées !");
-}
-
-var enregistrerClique = false;
-
-function test2() {
-  enregistrerClique = true;
-  alert("Informations enregistrées !" + enregistrerClique);
-}
-
-function test(enregistrerClique) {
+function confirmer(enregistrerClique) {
   if (enregistrerClique) {
-    if (choixPaiement === "") {
-      alert("Veuillez choisir un mode de paiement.");
-    } else {
-      alert("Commande confirmée avec " + choixPaiement);
-      window.location.href =
-        "https://projets.iut-orsay.fr/pizzeria/actions/creerCommande.php?";
-    }
+    alert("Commande confirmée avec ");
+    window.location.href =
+      "https://projets.iut-orsay.fr/pizzeria/actions/creerCommande.php?";
   } else {
     alert(
       "Veuillez enregistrer vos informations avant de confirmer la commande." +
--- src/main/php/view/vuePaiement.php
+++ src/main/php/view/vuePaiement.php
@@ -17,16 +17,8 @@
     </section>
     <section>
         <div class="formulaire">
-
-            <h3>Informations personnelles</h3>
+            <h2>Informations personnelles</h2>
             <form id="infos-personnelles" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-                <label for="nom">Nom :</label>
-                <input type="text" id="nom" name="nom"
-                    value="<?php echo isset($_POST['nom']) ? htmlspecialchars($_POST['nom']) : ''; ?>" required><br><br>
-                <label for="prenom">Prénom :</label>
-                <input type="text" id="prenom" name="prenom"
-                    value="<?php echo isset($_POST['prenom']) ? htmlspecialchars($_POST['prenom']) : ''; ?>"
-                    required><br><br>
                 <label for="adresse">Adresse :</label>
                 <input type="text" id="adresse" name="adresse"
                     value="<?php echo isset($_POST['adresse']) ? htmlspecialchars($_POST['adresse']) : ''; ?>"><br><br>
@@ -36,30 +28,54 @@
                 <label for="code-postal">Code Postal :</label>
                 <input type="text" id="code-postal" name="code-postal"
                     value="<?php echo isset($_POST['code-postal']) ? htmlspecialchars($_POST['code-postal']) : ''; ?>"><br><br>
-                <button type="submit" name="enregistrer" onclick="test2();">Enregistrer</button>
+
+                <h2>Choix du paiement</h2>
+                <div class="choix-paiement">
+                    <label>
+                        <input type="radio" name="paiement" value="Paiement à la caisse" id="paiement-cash-radio" <?php echo (isset($_POST['paiement']) && $_POST['paiement'] === 'Paiement à la caisse') ? 'checked' : ''; ?>>
+                        Paiement à la caisse
+                    </label>
+                    <label>
+                        <input type="radio" name="paiement" value="Paiement en ligne" id="paiement-ligne-radio" <?php echo (isset($_POST['paiement']) && $_POST['paiement'] === 'Paiement en ligne') ? 'checked' : ''; ?>>
+                        Paiement en ligne
+                    </label>
+                </div>
+
+                <button type="submit" name="enregistrer">Enregistrer</button>
             </form>
         </div>
+
     </section>
     <?php
+    $enregistrerClique = false;
     include("../controller/controllerPizzeria.php");
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $enregistrerClique = true;
         if (!empty($_POST['adresse']) || !empty($_POST['ville']) || !empty($_POST['code-postal'])) {
             if (empty($_POST['adresse']) || empty($_POST['ville']) || empty($_POST['code-postal'])) {
                 echo "<script>alert('Veuillez renseigner une adresse complète pour la livraison. Rien pas de livraison.');</script>";
+                $enregistrerClique = false;
             } else {
                 $destination = $_POST['adresse'] . ', ' . $_POST['ville'] . ', ' . $_POST['code-postal'] . ", France";
                 $distance = controllerPizzeria::calculDistancePizzeria($destination);
                 if ($distance > 30 || $distance == -1) {
                     echo "<script>alert('Vous êtes trop loin veuillez renseigner une autre adresse $distance minutes');</script>";
+                    $enregistrerClique = false;
                 } else {
                     $_SESSION['adresse'] = $_POST['adresse'];
                     $_SESSION['ville'] = $_POST['ville'];
                     $_SESSION['codePostal'] = $_POST['code-postal'];
-                    $ville = $_SESSION['ville'];
-                    $enregistrerClique = true;
-                    echo "<script>alert('Enregistré $distance minutes   $ville');</script>";
+                    echo "<script>alert('Enregistré $distance minutes');</script>";
                 }
             }
+        }
+
+        if (!isset($_POST['paiement'])) {
+            echo "<script>alert('Veuillez choisir un mode de paiement');</script>";
+            $enregistrerClique = false;
+        } else {
+            $_SESSION['modePaiement'] = $_POST['paiement'];
         }
     }
     ?>
@@ -70,13 +86,9 @@
             <?php
             require_once("../controller/controllerPizza.php");
 
-            // Assurez-vous que $_SESSION['panier'] et $_SESSION['tabQuantite'] existent et ne sont pas vides
             if (!empty($_SESSION['panier']) && !empty($_SESSION['tabQuantite'])) {
                 echo "<div class='recap-commande'>";
-
-                // Parcourir le panier et afficher les détails de chaque pizza
                 foreach ($_SESSION['panier'] as $idPizza) {
-                    // Récupérer les détails de la pizza
                     $pizzaDetails = controllerPizza::getPizzaPanier($idPizza);
                     if ($pizzaDetails->rowCount() > 0) {
                         while ($row = $pizzaDetails->fetch(PDO::FETCH_ASSOC)) {
@@ -84,11 +96,10 @@
                             $quantite = $_SESSION['tabQuantite'][$idPizza];
                             $prix = controllerPizza::getPrixPizza($idPizza);
 
-                            // Afficher les détails de la pizza dans le récapitulatif de la commande
                             echo "<p>Nom de la pizza : $nom</p>";
                             echo "<p>Quantité : $quantite</p>";
                             echo "<p>Prix unitaire : $prix €</p>";
-                            echo "<hr>"; // Ajouter une ligne pour séparer les pizzas
+                            echo "<hr>";
                         }
                     }
                 }
@@ -97,25 +108,20 @@
             }
             require_once("../controller/controllerProduit.php");
 
-            // Vérifiez si $_SESSION['panierProduit'] et $_SESSION['tabQuantiteProduit'] existent et ne sont pas vides
             if (!empty($_SESSION['panierProduit']) && !empty($_SESSION['tabQuantiteProduit'])) {
                 echo "<div class='recap-commande-produits'>";
-
-                // Parcourez le panier des produits et affichez les détails de chaque produit
                 foreach ($_SESSION['panierProduit'] as $idProduit) {
-                    // Récupérer les détails du produit
+
                     $produitDetails = controllerProduit::getProduitPanier($idProduit);
                     if ($produitDetails->rowCount() > 0) {
                         while ($row = $produitDetails->fetch(PDO::FETCH_ASSOC)) {
                             $nomProduit = htmlspecialchars($row['nomProduit']);
                             $quantiteProduit = $_SESSION['tabQuantiteProduit'][$idProduit];
                             $prixProduit = controllerProduit::getPrixProduit($idProduit);
-
-                            // Afficher les détails du produit dans le récapitulatif de commande
                             echo "<p>Nom du produit : $nomProduit</p>";
                             echo "<p>Quantité : $quantiteProduit</p>";
                             echo "<p>Prix unitaire : $prixProduit €</p>";
-                            echo "<hr>"; // Ajouter une ligne pour séparer les produits
+                            echo "<hr>";
                         }
                     }
                 }
@@ -123,6 +129,9 @@
                 echo "</div>";
             }
             ?>
+            <form method="post">
+                <button type="submit" name="annuler">Annuler</button>
+            </form>
         </div>
         <?php
 
@@ -130,22 +139,12 @@
             header('Location: ../actions/viderPanier.php');
         }
         ?>
-        <form method="post">
-            <button type="submit" name="annuler">Annuler</button>
-        </form>
-    </section>
-    <section>
-        <h2>Choix du paiement</h2>
-        <div class="choix-paiement">
-            <button id="paiement-cash" onclick="selectPayment('Paiement à la caisse')">Paiement à la caisse</button>
-            <button id="paiement-ligne" onclick="selectPayment('Paiement en ligne')">Paiement en ligne</button>
-        </div>
-    </section>
 
+    </section>
     <section>
         <?php
-        echo "<button id=\"confirmer\" onclick=\"test($enregistrerClique)\">Confirmer la commande</button>";
-        ?>
+        echo "<button id=\"confirmer\" onclick=\"confirmer($enregistrerClique)\">Confirmer la commande</button>"
+            ?>
     </section>
 </body>
 <?php include("../view/footer.html"); ?>
